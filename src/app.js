81501707fc9c67e6cdfd6d772a419da7cfbe716f
Alejo Passe <passealejo@gmail.com>
2026-05-23 18:32:32 -0300

    fix

--- src/app.js
+++ src/app.js
@@ -1,27 +1,27 @@
-let usuario=[];
+let usuarios=[];
 
 function crear(usuario) {
     usuario.push(usuario);
 }
 
 function listar() {
-    return usuario;
+    return usuarios;
 }
 
 function actualizar(id,model) {
-    const usuario = usuarios.find(c=>c.id===id);
+    let usuario = usuarios.find(c => c.id === id);
 
     if(usuario) {
         usuario.nombre = model.nombre;
         usuario.email = model.email;
-        usuario.contrasena =model.contrasena;
+        usuario.contrasena = model.contrasena;
     }
 
     usuarios.update(usuario);
 }
 
 function eliminar(id) {
-    var usuario = usuarios.filter(c => c.id !== id);
+    let usuario = usuarios.filter(c => c.id !== id);
 
     usuarios.delete(usuario);
 }
